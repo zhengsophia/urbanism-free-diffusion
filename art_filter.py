a45d8546ec026356133f972be9d9d3368b72cf44
Sophia Zheng <sjzheng@mit.edu>
2025-04-20 19:49:26 -0400

    dataset filtering

---
+++ art_filter.py
@@ -0,0 +1,236 @@
+from transformers import CLIPProcessor, CLIPModel
+import torch
+import numpy as np
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+from tqdm import tqdm
+from torch.utils.data import Dataset
+from PIL import Image
+import json
+import os
+from torch.utils.data import DataLoader
+
+class SamCaptionDataset(Dataset):
+    def __init__(self, image_dir, caption_json, get_img=True, get_cap=True, transform=None):
+        self.image_dir = image_dir
+        self.get_img = get_img
+        self.get_cap = get_cap
+        self.transform = transform
+
+        # Load the JSON with { "image_id": "caption" }
+        with open(caption_json, "r") as f:
+            self.data = json.load(f)
+        self.ids = list(self.data.keys())
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+        id = self.ids[idx]
+        sample = {"ids": id}
+
+        if self.get_img:
+            image_path = os.path.join(self.image_dir, f"{id}.jpg")  # or .png
+            image = Image.open(image_path).convert("RGB")
+            if self.transform:
+                image = self.transform(image)
+            sample["images"] = image
+
+        if self.get_cap:
+            caption = self.data[id]
+            sample["text"] = (caption,)
+
+        return sample
+
+
+class Caption_filter:
+    def __init__(self, filter_prompts=["urbanism", "urban planning", "city", "cities", "architecture", "building", "highway", "bridge",
+                                       "home", "landmark", "pedestrian", "crosswalks", "apartment", "road", "street", "house", "urban", "neighborhood", "skyscraper",
+                                       ],):
+        self.filter_prompts = filter_prompts
+        self.total_count=0
+        self.filter_count=[0]*len(filter_prompts)
+
+    def reset(self):
+        self.total_count=0
+        self.filter_count=[0]*len(self.filter_prompts)
+    def filter(self, captions):
+        filter_result = []
+        for caption in captions:
+            words = caption[0]
+            if words == None:
+                filter_result.append((True, "None"))
+                continue
+            words = words.lower()
+            words = words.split()
+            filt = False
+            reason=None
+            for i, filter_keyword in enumerate(self.filter_prompts):
+                key_len = len(filter_keyword.split())
+                for j in range(len(words)-key_len+1):
+                    if " ".join(words[j:j+key_len]) == filter_keyword:
+                        self.filter_count[i] += 1
+                        filt = True
+                        reason = filter_keyword
+                        break
+                if filt:
+                    break
+            filter_result.append((filt, reason))
+            self.total_count += 1
+        return filter_result
+    
+class Clip_filter:
+    prompt_threshold = {
+        "urbanism": 17,
+        "urban planning": 17.5,
+        "city": 19,
+        "cities": 15.8,
+        "architecture": 17,
+        "buildings": 15,
+        "highways": 19.2,
+        "bridges": 20,
+        "homes": 16.3,
+        "landmarks": 15,
+        "pedestrian": 18,
+        "crosswalks": 19,
+    }
+    
+@torch.no_grad()
+def __init__(self, positive_prompt=["urbanism", "urban planning", "city", "cities", "architecture", "buildings", "highways", "bridges",
+                                    "homes", "landmarks", "pedestrian", "crosswalks"
+                                    ],
+              device="cuda"):
+    self.device = device
+    self.model = (CLIPModel.from_pretrained("openai/clip-vit-large-patch14")).to(device)
+    self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+    self.positive_prompt = positive_prompt
+    self.text = self.positive_prompt
+    self.tokenizer = self.processor.tokenizer
+    self.image_processor = self.processor.image_processor
+    self.text_encoding = self.tokenizer(self.text, return_tensors="pt", padding=True).to(device)
+    self.text_features = self.model.get_text_features(**self.text_encoding)
+    self.text_features = self.text_features / self.text_features.norm(p=2, dim=-1, keepdim=True)
+@torch.no_grad()
+def similarity(self, image):
+    # inputs = self.processor(text=self.text, images=image, return_tensors="pt", padding=True)
+    image_processed = self.image_processor(image, return_tensors="pt", padding=True).to(self.device, non_blocking=True)
+    inputs = {**self.text_encoding, **image_processed}
+    outputs = self.model(**inputs)
+    logits_per_image = outputs.logits_per_image
+    return logits_per_image
+
+def get_logits(self, image):
+    logits_per_image = self.similarity(image)
+    return logits_per_image.cpu()
+
+def get_image_features(self, image):
+    image_processed = self.image_processor(image, return_tensors="pt", padding=True).to(self.device, non_blocking=True)
+    image_features = self.model.get_image_features(**image_processed)
+    return image_features
+
+class Urban_filter:
+    def __init__(self):
+        self.caption_filter = Caption_filter()
+        self.clip_filter = Clip_filter()
+    def caption_filt(self, dataloader):
+        self.caption_filter.reset()
+        dataloader.dataset.get_img = False
+        dataloader.dataset.get_cap = True
+        remain_ids = []
+        filtered_ids = []
+        for i, batch in tqdm(enumerate(dataloader)):
+            captions = batch["text"]
+            filter_result = self.caption_filter.filter(captions)
+            for j, (filt, reason) in enumerate(filter_result):
+                if filt:
+                    filtered_ids.append((batch["ids"][j], reason))
+                    if i%10==0:
+                        print(f"Filtered caption: {captions[j]}, reason: {reason}")
+                else:
+                    remain_ids.append(batch["ids"][j])
+        return {"remain_ids":remain_ids, "filtered_ids":filtered_ids, "total_count":self.caption_filter.total_count, "filter_count":self.caption_filter.filter_count, "filter_prompts":self.caption_filter.filter_prompts}
+
+    def clip_filt(self, clip_logits_ckpt:dict):
+        logits = clip_logits_ckpt["clip_logits"]
+        ids = clip_logits_ckpt["ids"]
+        text = clip_logits_ckpt["text"]
+        filt_mask = torch.zeros(logits.shape[0], dtype=torch.bool)
+        for i, prompt in enumerate(text):
+            threshold = Clip_filter.prompt_threshold[prompt]
+            filt_mask = filt_mask | (logits[:,i] >= threshold)
+        filt_ids = []
+        remain_ids = []
+        for i, id in enumerate(ids):
+            if filt_mask[i]:
+                filt_ids.append(id)
+            else:
+                remain_ids.append(id)
+        return {"remain_ids":remain_ids, "filtered_ids":filt_ids}
+
+    def clip_feature(self, dataloader):
+        dataloader.dataset.get_img = True
+        dataloader.dataset.get_cap = False
+        clip_features = []
+        ids = []
+        for i, batch in enumerate(dataloader):
+            images = batch["images"]
+            features = self.clip_filter.get_image_features(images).cpu()
+            clip_features.append(features)
+            ids.extend(batch["ids"])
+        clip_features = torch.cat(clip_features)
+        return {"clip_features":clip_features, "ids":ids}
+
+
+    def clip_logit(self, dataloader):
+        dataloader.dataset.get_img = True
+        dataloader.dataset.get_cap = False
+        clip_features = []
+        clip_logits = []
+        ids = []
+        for i, batch in enumerate(dataloader):
+            images = batch["images"]
+            # logits = self.clip_filter.get_logits(images)
+            feature = self.clip_filter.get_image_features(images)
+            logits = self.clip_logit_by_feat(feature)["clip_logits"]
+
+            clip_features.append(feature)
+            clip_logits.append(logits)
+            ids.extend(batch["ids"])
+
+        clip_features = torch.cat(clip_features)
+        clip_logits = torch.cat(clip_logits)
+        return {"clip_features":clip_features, "clip_logits":clip_logits, "ids":ids, "text": self.clip_filter.text}
+
+    def clip_logit_by_feat(self, feature):
+        feature = feature.clone().to(self.clip_filter.device)
+        feature = feature / feature.norm(p=2, dim=-1, keepdim=True)
+        logit_scale = self.clip_filter.model.logit_scale.exp()
+        logits = ((feature @ self.clip_filter.text_features.T) * logit_scale).cpu()
+        return {"clip_logits":logits, "text": self.clip_filter.text}
+    
+if __name__ == "__main__":
+    import pickle
+    dataset = SamCaptionDataset(
+    image_dir="/path/to/sam/images",
+    caption_json="/path/to/captions.json",
+)
+
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=False)
+    # with open("/vision-nfs/torralba/scratch/jomat/sam_dataset/filt_result/sa_000000/clip_logits_result.pickle","rb") as f:
+    #     result=pickle.load(f)
+    # feat = result['clip_features']
+
+    urban_filter = Urban_filter()
+
+    # Filter based on captions
+    caption_filter_results = urban_filter.caption_filt(dataloader)
+    print(caption_filter_results)
+
+    # Filter based on CLIP visual logits
+    clip_logit_results = urban_filter.clip_logit(dataloader)
+    clip_filter_results = urban_filter.clip_filt(clip_logit_results)
+    print(clip_filter_results)
+    
+    # logits = Urban_filter().clip_logit_by_feat(dataloader)
+    # print(logits)
+

026e3988598a4c06801b1d00ebbda1ffb0e45397
Apprisco <34324909+Apprisco@users.noreply.github.com>
2025-05-05 13:08:21 -0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -33,6 +33,21 @@ class SA1BDataset(Dataset):
         cap_path = self.cap_dir/f"{sa_id}.txt"
         caption = cap_path.read_text(encoding="utf-8").strip()
         return {"id": sa_id, "image": image, "caption": caption}
+        
+class SA1BDatasetFinetune(SA1BDataset):
+    def __init__(
+        self,
+        ids_pkl: Path,
+        ids_pkl2: Path,
+        transform: Optional[transforms.Compose] = None,
+    ):
+        self.ids = pickle.load(open(ids_pkl, "rb"))+pickle.load(open(ids_pkl2, "rb"))
+        self.img_dir = Path("SamDataset/images/")
+        self.cap_dir = Path("SamDataset/captions/")
+        self.transform = transform or transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+        ])
 
 def get_dataloader(
     topic: str,
@@ -44,6 +59,27 @@ def get_dataloader(
     ids_pkl = Path(f"SamDataset/ids/{topic}_ids.pkl")
     ds = SA1BDataset(
         ids_pkl=ids_pkl,
+        transform=transform,
+    )
+    return DataLoader(
+        ds,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    
+def get_finetuneloader(
+    topic: str,
+    batch_size: int = 32,
+    shuffle: bool = True,
+    num_workers: int = 8,
+    transform: Optional[transforms.Compose] = None,
+) -> DataLoader:
+    ids_pkl = Path(f"SamDataset/ids/{topic}_ids.pkl")
+    ds = SA1BDataset(
+        ids_pkl=Path(f"SamDataset/ids/filtered_ids.pkl")
+        ids_pkl2=ids_pkl,
         transform=transform,
     )
     return DataLoader(

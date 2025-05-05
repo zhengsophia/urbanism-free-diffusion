7f5aea58506d8658ee6cbc378ff39436c82096f0
Apprisco <34324909+Apprisco@users.noreply.github.com>
2025-05-05 13:23:55 -0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -76,7 +76,7 @@ def get_finetuneloader(
     num_workers: int = 8,
     transform: Optional[transforms.Compose] = None,
 ) -> DataLoader:
-    ids_pkl = Path(f"SamDataset/ids/{topic}_ids.pkl")
+    ids_pkl = Path("SamDataset/ids/{topic}_ids.pkl")
     ds = SA1BDatasetFinetune(
         ids_pkl=Path(f"SamDataset/ids/filtered_ids.pkl")
         ids_pkl2=ids_pkl,
